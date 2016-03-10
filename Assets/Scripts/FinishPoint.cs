0aa70f6977aa938bd348ac1eb718282349500c5d
jerry111zhang <jerry111zhang@163.com>
2016-03-09 20:38:10 -0500

    added jb

---
+++ Assets/Scripts/FinishPoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FinishPoint : MonoBehaviour {
+
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col) {
+		text.text = "You win!";
+	}
+}
