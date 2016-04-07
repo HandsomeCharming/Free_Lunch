5f9b76056e945ad1694c50ad1518373638322044
jerry111zhang <jerry111zhang@163.com>
2016-04-07 03:12:09 -0400

    amigo

--- Assets/Scripts/HealthAndChargeBar.cs
+++ Assets/Scripts/HealthAndChargeBar.cs
@@ -5,10 +5,18 @@ public class HealthAndChargeBar : MonoBehaviour {
 
 	public Character character;
 
-	public GameObject chargeBar;
+	public Texture2D chargeBar;
+
+	public int cursorSize = 63;
+	int sizeX;
+	int sizeY;
+
+	void Awake() {
+		sizeX = cursorSize;
+		sizeY = cursorSize;
+	}
 
 	void Start () {
-		chargeBar = ((GameObject)Instantiate(Resources.Load("Prefabs/Chargebar")));
 	}
 
 	void Update () {
@@ -19,10 +27,18 @@ public class HealthAndChargeBar : MonoBehaviour {
 		transform.position = character.transform.position - new Vector3(3f,0,0);
 		transform.localScale = new Vector3(3,4*(character.status.hp/character.status.maxhp),3);
 		if(InputHandler.current.chargingTime > 0) {
-			chargeBar.transform.position = character.transform.position + new Vector3(3f,0,0);
-			chargeBar.transform.localScale =  new Vector3(3,4*(InputHandler.current.chargingTime/character.chargedAttackModifier.chargeTime),3);
-		} else {
-			chargeBar.transform.localScale =  new Vector3(3,0,3);
+			
+		}
+	}
+
+	void OnGUI()
+	{
+		if(InputHandler.current.chargingTime > 0) {
+			
+			float lerp = (InputHandler.current.chargingTime)/character.chargedAttackModifier.chargeTime;
+			float scale = Mathf.Lerp(cursorSize, 20, lerp);
+
+			GUI.DrawTexture(new Rect(Event.current.mousePosition.x - (scale/2), Event.current.mousePosition.y - (scale/2), scale, scale), chargeBar);
 		}
 	}
 }
--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,6 +4,20 @@ using System.Collections;
 public class LevelController : MonoBehaviour {
 	public float playerSpawnPosX;
 	public float playerSpawnPosY;
+
+	public Vector2 cursurHotspot = Vector2.zero;
+	public Texture2D cursorTexture;
+	public CursorMode cursorMode;
+
+	public int cursorSize = 63;
+	int sizeX;
+	int sizeY;
+
+	void Awake() {
+		sizeX = cursorSize;
+		sizeY = cursorSize;
+	}
+
 	// Use this for initialization
 	void Start () {
 		Projectile.InitProjectileData();
@@ -18,6 +32,9 @@ public class LevelController : MonoBehaviour {
 		AIController.current.player = player;
 		Vector3 pos = new Vector3 (playerSpawnPosX, 0, playerSpawnPosY);		
 		player.gameObject.transform.position = pos;
+
+		//Cursor.SetCursor(cursorTexture, cursurHotspot, cursorMode);
+		Cursor.visible = false;
 	}
 
 	Character loadPlayer() {
@@ -27,5 +44,10 @@ public class LevelController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(Event.current.mousePosition.x - (cursorSize/2), Event.current.mousePosition.y - (cursorSize/2), sizeX, sizeY), cursorTexture);
 	}
 }
