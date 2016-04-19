615a6555ea6070baf3707d3c16feea5d7ad42937
jerry111zhang <jerry111zhang@163.com>
2016-04-19 01:52:32 -0400

    tried drag ui

---
+++ Assets/Scripts/DragableUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragableUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnDrag() { 
+		transform.position = Input.mousePosition; 
+	}
+}
